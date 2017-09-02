319bb4c0a3a3f4c1b7cb2e5abed24baf48916b46
androidlover5842 <androidlover5842@gmail.com>
2017-09-03 02:58:56 +0530

    grandneove3g: update libs

--- extract-files.sh
+++ extract-files.sh
@@ -1,18 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-VENDOR=samsung
-DEVICE=grandneove3g
+export VENDOR=samsung
+export DEVICE=grandneove3g
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
 
-echo "Pulling device files..."
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+    FILE=${PARSING_ARRAY[0]}
+    DEST=${PARSING_ARRAY[1]}
+    if [ -z $DEST ]
+    then
+        DEST=$FILE
+    fi
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
-    cp ../../../../g531h/stock/$FILE $BASE/$FILE
-    echo "$FILE pulled"
+
+    if [ -z "$STOCK_ROM_DIR" ]; then
+        adb pull /system/$FILE $BASE/$DEST
+    else
+        cp $STOCK_ROM_DIR/$FILE $BASE/$DEST
+    fi
+
+    # if file does not exist try destination
+    if [ "$?" != "0" ]
+    then
+        adb pull /system/$DEST $BASE/$DEST
+    fi
 done
 
 ./setup-makefiles.sh
